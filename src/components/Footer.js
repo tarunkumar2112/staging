4fbbec1a9845a7ce46e3e10af48dc90ee5701a34
Arman <65326024+tess-ss@users.noreply.github.com>
2024-03-22 23:11:17 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,11 +31,7 @@ const Footer = () => {
         {/* Social Media Icons */}
         <div className="flex space-x-4">
           {/* Replace the src attribute with the URLs of your social media icons */}
-          <a href="#" target="_blank"><img src={Facebook} alt="Social Media Icon" className="h-6 w-6" /></a>
-          <a href="#" target="_blank"><img src={Insta} alt="Social Media Icon" className="h-6 w-6" /></a>
           <a href="https://x.com/ArmanSameer95" target="_blank"><img src={Twitter} alt="Social Media Icon" className="h-6 w-6" /></a>
-          <a href="#" target="_blank"><img src={Tiktok} alt="Social Media Icon" className="h-6 w-6" /></a>
-          <a href="#" target="_blank"><img src={Youtube} alt="Social Media Icon" className="h-6 w-6" /></a>
 
         </div>
       </div>
