1efaa99301b8336f52ac9586ab6b29ca80317e78
Arman <65326024+tess-ss@users.noreply.github.com>
2024-04-08 01:32:36 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -276,7 +276,7 @@ const Home = () => {
             3rd Party Risk Management</li>
             <li class="flex items-start gap-[10px;] font-[16px] py-[5px]">
             <img className='mt-[2px]' src={checkmark} alt="checkmark"/>
-            Pentest</li
+            Pentest</li>
           </ul>
             <button className="bg-[#1E1333] text-sm transition-all hover:bg-[#DBD9DE] text-[rgba(124, 124, 124, 1)] hover:text-[#2E1C63] font-medium py-2 px-4 rounded-[40px] border border-solid border-slate-400/40 hover:border-white mt-[30px]">Purchase Now</button>
           </div>
